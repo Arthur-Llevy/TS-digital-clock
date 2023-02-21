1ae8bdb1421dcfe4525a8da1e6e1157d9aff1ba6
Arthur Levy <arthurlbvieiraa@gmail.com>
2023-02-20 21:37:49 -0300

    Correction of the time format.

--- main.js
+++ main.js
@@ -3,20 +3,39 @@ let currentDate = document.getElementById('current-date');
 let currentTime = document.getElementById('current-time');
 let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'Octuber', 'November', 'December'];
 let currentMonth = date.getMonth();
-let currentDay = date.getDay();	
+let currentDay = date.getDate();	
 let currentYear = date.getFullYear();
 
-currentDate.innerHTML = `${months[currentMonth]} - ${currentDay}/${currentYear}`
+currentDay < 10 ? 
+	currentDate.innerHTML = `${months[currentMonth]} - 0${currentDay}/${currentYear}` :
+	currentDate.innerHTML = `${months[currentMonth]} - ${currentDay}/${currentYear}`;
 
 setInterval(() => {
 
-	let date = new Date();		
+	let date = new Date();	
 
-	if (date.getHours <= 12) {		
-		currentTime.innerHTML = `${date.getHours()}/${date.getMinutes()}/${date.getSeconds()} am`
-	}else {
-		currentTime.innerHTML = `${date.getHours() - 12}/${date.getMinutes()}/${date.getSeconds()} pm`
-	};
+	let currentHours = date.getHours() < 10  ? `0${date.getHours()}` : `${date.getHours()}`;
+	let currentMinutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : `${date.getMinutes()}`;
+	let currentSeconds = date.getSeconds() < 10 ? `0${date.getSeconds()}` : `${date.getSeconds()}`;
+
+
+	if (date.getHours() < 12 && date.getHours() < 10) {
+
+		currentTime.innerHTML = `0${currentHours}:${currentMinutes}:${currentSeconds} am`; 
+
+	}else if (date.getHours() < 12 && date.getHours() >= 10) {
+		
+		currentTime.innerHTML = `${currentHours}:${currentMinutes}:${currentSeconds} am`; 
+
+	}else if (date.getHours() >= 12 && date.getHours() - 12 < 10){
+
+		currentTime.innerHTML = `0${currentHours - 12}:${currentMinutes}:${currentSeconds} pm`;
+
+	}else if (date.getHours() >= 12 && date.getHours()  - 12 > 10){
+
+		currentTime.innerHTML = `${currentHours - 12}:${currentMinutes}:${currentSeconds} pm`; 
+
+	}
 
 }, 1000);
 
